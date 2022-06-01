429da6f5e2db546f488a6b130527e84deacb0b9b
Kairo Ribeiro <kairo10@gmail.com>
2022-05-31 23:36:52 -0400

    complete lab

--- js/app.js
+++ js/app.js
@@ -1,1 +1,24 @@
-console.log('sanity check')
+
+// Cached Elements
+
+const input = document.getElementById('input')
+const button = document.getElementById('submit-button')
+const toDoList = document.getElementById('todo-list')
+
+// Event Listeners
+
+button.addEventListener('click', function(event) {
+    if(input.value === "") 
+    return 
+    const newTodo = document.createElement('li')
+    newTodo.textContent = input.value
+    toDoList.appendChild(newTodo)
+    input.value = ''
+    
+})
+
+toDoList.addEventListener('click', function(event) {
+    const parentNode = event.target.parentNode
+    parentNode.removeChild(event.target)
+  })
+
